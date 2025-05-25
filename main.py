b9ccbe9722f73ff07e7d784e5a51feb6dec9e2a7
Mayegg <1498638223@qq.com>
2025-05-25 12:33:51 +0800

    structure tuning

--- main.py
+++ main.py
@@ -315,6 +315,6 @@ class ImageViewerApp:
 if __name__ == "__main__":
     root = tk.Tk()
     # 添加图标设置（新增代码）
-    root.iconbitmap("app.ico")  # 路径为项目根目录下的app.ico文件
+    root.iconbitmap("resources/app.ico")  # 路径为项目根目录下的app.ico文件
     app = ImageViewerApp(root)
     root.mainloop()
